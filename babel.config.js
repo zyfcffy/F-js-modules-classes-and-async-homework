c79a0b3209c89f71f208107b15025f4a1b2d1160
kevindongzg <dongkevin94@gmail.com>
2020-06-04 23:08:35 +0800

    update

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ["@babel/preset-env", "airbnb"],
-  env: {
-    test: {
-      presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-    },
-  },
-  plugins: ["@babel/plugin-proposal-class-properties"],
-};
